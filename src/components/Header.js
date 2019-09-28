7203f06f96d4ea97bf71be72120cbd91759360cc
chaharshivam <chahar.shivam@yahoo.com>
2019-09-28 22:34:27 +0530

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+        <header className="header">
+            <div className="wrapper">
+                <nav className="navbar flex-between">
+                    <a className="logo" href='#'><span>GitHub</span>Jobs</a>
+                    <label htmlFor="toggle">
+                        <div className="menu">MENU</div>
+                    </label>
+                    <input type="checkbox" id="toggle"></input>
+                    <ul className="nav-list flex-between">
+                        <li className="nav-item">
+                            <a href="#">Bookmarks</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#">Contribute</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#">About</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
