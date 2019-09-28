2cd13f6fdcc8ecf104db609ebe5f814a42ae3687
chaharshivam <chahar.shivam@yahoo.com>
2019-09-28 22:35:03 +0530

    Add Loader

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Loader(props) {
+    return (
+        <div class="lds-ripple">
+            <div></div>
+            <div></div>
+        </div>
+    );
+}
+
+export default Loader;
