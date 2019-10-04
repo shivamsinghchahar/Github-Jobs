1e3df0c82d709e35dc3c59fd15f9e44b3560c51b
chaharshivam <chahar.shivam@yahoo.com>
2019-10-04 23:27:15 +0530

    Update: state & pagination

--- src/components/Job/Job.js
+++ src/components/Job/Job.js
@@ -2,54 +2,65 @@ import React from 'react';
 import JobCard from './JobCard';
 
 class Job extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            pages: 0,
-            current: 1
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      pages: 0,
+      current: 0
     }
+  }
 
-    componentDidMount() {
-        const pages = Math.ceil(this.props.jobs.length / 10);
+  componentDidMount() {
+    const pages = Math.ceil(this.props.jobs.length / 10);
 
-        this.setState({ ...this.state, pages });
-    }
+    this.setState({ pages });
+  }
 
-    handlePage = current => {
-        this.setState({ ...this.state, current })
-    }
+  handlePage = current => {
+    this.setState({ current })
+  }
 
-    render() {
-        return (
-            <section className="jobs-container flex-start">
-                <div className="wrapper flex-between">
-                    {
-                        this.props.jobs && this.props.jobs.slice((this.state.current - 1) * 10, 10 + (this.state.current - 1) * 10).map(job => {
-                          return  (
-                                <JobCard 
-                                    job={job}
-                                />
-                            )
-                        })
-                    }
-                </div>
-                <div className="paginate flex-start wrapper">
+  jobsPerPage = () => {
+    const { pages, current } = this.state;
+    return this.props.jobs.slice(
+      (this.state.current * 10),
+      (this.state.current + 1) * 10
+    );
+  }
 
-                    {
-                        new Array(this.state.pages).fill(0).map((page, i) => {
-                           return (<button 
-                                className={`page-btn ${this.state.current == i+1 ? 'active-page': ''}`}
-                                onClick={() => this.handlePage(i+1)}
-                            >
-                                { i+1 }
-                            </button>)
-                        })
-                    }
-                </div>
-            </section>
-        );
-    }
+  render() {
+    return (
+      <section className="jobs-container flex-start">
+        <div className="wrapper flex-between">
+          {
+            this.props.jobs && this.jobsPerPage().map(job => {
+              return (
+                <JobCard
+                  job={job}
+                  key={job.id}
+                />
+              )
+            })
+          }
+        </div>
+        <div className="paginate flex-start wrapper">
+          <button className="prev-jobs" onClick={this.props.prevFifty}>Prev 50</button>
+          {
+            new Array(this.state.pages).fill(0).map((page, i) => {
+              return (<button
+                className={`page-btn ${this.state.current == i ? 'active-page' : ''}`}
+                onClick={() => this.handlePage(i)}
+                key={i}
+              >
+                {i + 1}
+              </button>)
+            })
+          }
+          <button className="next-jobs" onClick={this.props.nextFifty}>Next 50</button>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Job;
