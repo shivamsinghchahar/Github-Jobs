f6403d0ecce93386c0e3e235b01a3d7ebe6861cb
chaharshivam <chahar.shivam@yahoo.com>
2020-04-27 16:50:33 +0530

    Update Input styles

--- src/components/shared/Input.js
+++ src/components/shared/Input.js
@@ -6,7 +6,7 @@ function Input({ type, placeholder = "", handleChange, value, name, dark }) {
     <input
       name={name}
       type={type}
-      className={`${dark ? 'placeholder-white text-white bg-blue-800 border-2 border-blue-800 focus:bg-blue-600 focus:border-blue-700' : 'border-2 border-white focus:bg-gray-100 focus:border-blue-500'} w-full my-1 lg:my-2 shadow rounded py-2 px-4 appearance-none focus:outline-none`}
+      className={`${dark ? 'placeholder-gray-400 text-white bg-blue-800 border-2 border-blue-800 focus:bg-blue-700 focus:border-blue-700' : 'border-2 border-white focus:bg-gray-100 focus:border-blue-500'} w-full my-1 lg:my-2 shadow rounded py-2 px-4 appearance-none focus:outline-none`}
       placeholder={placeholder}
       value={value}
       onChange={e => handleChange(e.target.name, e.target.value)}
