b54a72c27d4e2be035fffbfc733e9b95c96f4092
Shivam Chahar <chahar.shivam@yahoo.com>
2020-04-28 21:54:18 +0530

    Added footer (#8)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import GitHubButton from 'react-github-btn';
 
 function Footer({ dark }) {
   return (
-    <footer className={`${dark ? 'text-gray-300' : 'text-gray-800'} w-full flex justify-center items-center bottom-0 p-4`}>
-      <p className="font-mono text-xs">
-        Made with <i class="fas fa-heart text-red-500"></i> by <a target="_blank" href="https://twitter.com/shahr_96" className={`${dark ? 'text-blue-400' : 'text-blue-600'} italic`}>Shivam Chahar</a>
-      </p>
+    <footer className={`${dark ? 'text-gray-300 bg-blue-800' : 'text-gray-800 bg-gray-100'} w-full flex justify-center items-center border-1 bottom-0 p-4`}>
+      <section className="w-full lg:w-1/2">
+        <nav>
+          <ul className="w-full flex justify-between pt-4 pb-6 text-sm">
+            <li>
+              <a
+                className="hover:text-blue-400"
+                target="_blank"
+                href="https://github.com/chaharshivam/Github-Jobs"
+              >
+                Contribute
+            </a>
+            </li>
+            <li>
+              <a
+                className="hover:text-blue-400"
+                target="_blank"
+                href="https://github.com/chaharshivam/Github-Jobs/issues"
+              >
+                Report a bug
+              </a>
+            </li>
+            <li>
+              <GitHubButton
+                href="https://github.com/chaharshivam/Github-Jobs"
+                data-color-scheme={`no-preference: light; light: ${dark ? 'dark' : 'light'}; dark: dark;`}
+                data-icon="octicon-star"
+                data-show-count="true"
+                aria-label="Star chaharshivam/Github-Jobs on GitHub"
+              >
+                Star
+              </GitHubButton>
+            </li>
+          </ul>
+        </nav>
+        <p className="font-mono text-xs text-center">
+          Made with <i className="fas fa-heart text-red-500"></i> by <a target="_blank" href="https://twitter.com/shahr_96" className={`${dark ? 'text-blue-400' : 'text-blue-600'} italic`}>Shivam Chahar</a>
+        </p>
+      </section>
     </footer>
   );
 }
