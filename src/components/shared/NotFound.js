0506c4e0f8efe67ad4acf3e8f1d5a472e4279edb
Shivam Chahar <chahar.shivam@yahoo.com>
2020-04-28 23:33:41 +0530

    Add error screen (#9)

---
+++ src/components/shared/NotFound.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import notFound from '../../assets/images/404.svg';
+
+function NotFound({ dark }) {
+  return (
+    <section className="w-full flex flex-wrap items-center justify-center p-4">
+      <p className={`w-full sm:text-sm lg:text-xl antialiased font-mono text-center mb-4 py-4 ${dark ? 'text-gray-300' : 'text-gray-700'}`}>
+        Oops! Nothing here.
+        Please try again.
+      </p>
+      <div className="w-full flex justify-center mb-4">
+        <img className="sm:w-screen lg:w-1/3" src={notFound} alt="not found" />
+      </div>
+    </section>
+  );
+}
+
+const mapStateToProps = state => ({
+  dark: state.theme.dark,
+})
+
+export default connect(mapStateToProps)(NotFound);
