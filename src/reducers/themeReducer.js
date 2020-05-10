ea64f4ad094d5e603e19e79ffd93be18cdbc6978
chaharshivam <chahar.shivam@yahoo.com>
2020-05-10 17:44:41 +0530

    Make themes persistent

--- src/reducers/themeReducer.js
+++ src/reducers/themeReducer.js
@@ -1,14 +1,16 @@
 import * as actions from '../actions/themeActions';
 
 export const initialState = {
-  dark: false,
+  dark: localStorage.dark ? JSON.parse(localStorage.dark) : false,
 };
 
 export default function themeReducer(state = initialState, action) {
   switch(action.type) {
     case actions.LIGHT:
+      localStorage.setItem('dark', false);
       return { dark: false }
     case actions.DARK:
+      localStorage.setItem('dark', true);
       return { dark: true }
     default:
       return state;
