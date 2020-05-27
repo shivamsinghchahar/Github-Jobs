41f4a2b20f7893a8ffcf78fc589b744d1b615c64
Shivam Chahar <chahar.shivam@yahoo.com>
2020-05-27 12:44:56 +0530

    Added jobActions

--- src/actions/jobActions.js
+++ src/actions/jobActions.js
@@ -4,6 +4,7 @@ const baseUrl = "https://jobs.github.com/positions";
 export const GET_JOB = "GET_JOB";
 export const GET_JOB_SUCCESS = "GET_JOB_SUCCESS";
 export const GET_JOB_FAILURE = "GET_JOB_FAILURE";
+export const CLEAR_JOB = "CLEAR_JOB";
 
 export const getJob = () => ({
   type: GET_JOB,
@@ -16,6 +17,10 @@ export const getJobSuccess = (job) => ({
 
 export const getJobFailure = () => ({
   type: GET_JOB_FAILURE,
+});
+
+export const clearJob = () => ({
+  type: CLEAR_JOB,
 });
 
 export function fetchJob(id) {
