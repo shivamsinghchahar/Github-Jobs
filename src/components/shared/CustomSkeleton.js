4a606d04c9870901ab679e2229d77b8b32f42156
Shivam Chahar <chahar.shivam@yahoo.com>
2020-05-27 12:48:54 +0530

    Updated custom skeleton to have circle prop

--- src/components/shared/CustomSkeleton.js
+++ src/components/shared/CustomSkeleton.js
@@ -2,13 +2,13 @@ import React from "react";
 import { connect } from "react-redux";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
-function CustomSkeleton({ width, height, dark }) {
+function CustomSkeleton({ width, height, dark, count, circle }) {
   return (
     <SkeletonTheme
       color={dark ? "#2a4365" : ""}
       highlightColor={dark ? "#2c5282" : ""}
     >
-      <Skeleton width={width} height={height} />
+      <Skeleton width={width} height={height} count={count} circle={circle} />
     </SkeletonTheme>
   );
 }
